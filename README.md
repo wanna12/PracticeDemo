9be5cb740e4554e27388272a00a2a7da44226c51
wanna12 <jinyi_0612@163.com>
2021-03-28 00:42:48 +0800

    更改readme文件，补充ChecekActionFilter

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # PracticeDemo
 my practice repository
+
+1. 跨域问题    
+    1.1  CORS
+        设置请求头：Access-Control-Allow-Origin、Access-Control-Allow-Methods、Access-Control-Allow-Headers  
+    1.2  nginx转发（不会）
+2. 在webapi中设置浏览器cookie失败了，response.header中有set-cookie，但是memory中没有，为啥
+3. log4net并发问题，待验证
+4. logFilter和CheckActionFilter都需要完善
+5. CheckActionFilter只校验加了对应特性的接口
+6. 应增加一个校验参数的filter，校验参数是否合法
+7. springboot项目部署问题
---
+++ WebApplication.WebApi/WebApplication.WebApi/Filter/CheckActionFilter.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using Newtonsoft.Json;
+using WebApplication.Common.model;
+using WebApplication.Common.util;
+using WebApplication.ServiceBll.Model;
+
+namespace WebApplication.WebApi.Filter
+{
+    public class CheckActionFilter:ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            IEnumerable<string> sessions;
+            string[] pathStrings = actionContext.Request.RequestUri.AbsolutePath.Split('/');
+            string method = pathStrings[pathStrings.Length - 1];
+            if (!"getSession".Equals(method))
+            {
+                if (actionContext.Request.Headers.TryGetValues("em_session", out sessions))
+                {
+                    string session = sessions.FirstOrDefault();
+                    if (string.IsNullOrWhiteSpace(session) ||
+                        !session.Equals(SimpleCacheHelper<string>.GetCache("session")))
+                    {
+                        BasicModelResponse<string> response = new BasicModelResponse<string>()
+                        {
+                            ErrCode = -1,
+                            Status = -2,
+                            Data = new List<string>() {"session过期"}
+                        };
+                        HttpResponseMessage responseMessage = new HttpResponseMessage();
+                        responseMessage.Content = new StringContent(JsonConvert.SerializeObject(response));
+                        responseMessage.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                        actionContext.Response = responseMessage;
+                        return;
+                    }
+
+                    base.OnActionExecuting(actionContext);
+                }
+                else
+                {
+                    BasicModelResponse<string> response = new BasicModelResponse<string>()
+                    {
+                        ErrCode = -1,
+                        Status = -2,
+                        Data = new List<string>() {"session过期"}
+                    };
+                    HttpResponseMessage responseMessage = new HttpResponseMessage();
+                    responseMessage.Content = new StringContent(JsonConvert.SerializeObject(response));
+                    responseMessage.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                    actionContext.Response = responseMessage;
+                }
+            }
+            base.OnActionExecuting(actionContext);
+        }
+
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            var request = actionExecutedContext.Request;
+            string[] pathStrings = request.RequestUri.AbsolutePath.Split('/');
+            string method = pathStrings[pathStrings.Length - 1];
+            if ("getSession".Equals(method))
+            {
+                //如果调用的getsession，设置response的header
+                var response = actionExecutedContext.Response;
+                HttpContent responseContent = response.Content;
+                string session = SimpleCacheHelper<string>.GetCache("session");
+                response.Headers.Add("em_session",session);
+                HttpCookie cookie = new HttpCookie("em_session", session);
+                cookie.HttpOnly = true;
+                cookie.Secure = true;
+                cookie.SameSite = SameSiteMode.Lax;
+                //cookie.Domain = "192.168.*";
+                HttpContext.Current.Response.Cookies.Add(cookie);
+            }
+            base.OnActionExecuted(actionExecutedContext);
+        }
+    }
+}
